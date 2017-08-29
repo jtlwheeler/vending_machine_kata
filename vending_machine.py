ef1dda15feffa8ec16c05c9e6677df7e5ac48be6
John Wheeler <jtlwheeler@gmail.com>
2017-08-29 18:54:42 -0400

    First vending machine commit

---
+++ vending_machine.py
@@ -0,0 +1,4 @@
+class VendingMachine():
+    def is_valid_coin(self, coin):
+        if coin == "NICKEL":
+            return True
---
+++ vending_machine_tests.py
@@ -0,0 +1,7 @@
+import unittest
+from vending_machine import VendingMachine
+
+class VendingMachineTest(unittest.TestCase):
+    def test_is_valid_coin_with_nickel_should_return_true(self):
+        machine = VendingMachine()
+        self.assertTrue(machine.is_valid_coin("NICKEL"))
