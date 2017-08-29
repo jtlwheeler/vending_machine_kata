288b68d4a539e8e8b30c91e3d58424bb49bc8fb2
John Wheeler <jtlwheeler@gmail.com>
2017-08-29 19:25:18 -0400

    Added coin values.

--- vending_machine.py
+++ vending_machine.py
@@ -12,7 +12,8 @@ QUARTER = "QUARTER"
 class VendingMachine():
     """Represents a vending machine."""
 
-    VALID_COINS = [NICKEL, DIME, QUARTER]
+    #VALID_COINS = [NICKEL, DIME, QUARTER]
+    VALID_COINS = {NICKEL : 0.05, DIME : 0.10, QUARTER : 0.25}
 
     def is_valid_coin(self, coin):
         """Returns True if the coin is a valid and acceptable coin."""
--- vending_machine_tests.py
+++ vending_machine_tests.py
@@ -5,7 +5,7 @@
 """
 
 import unittest
-import vending_machine
+import vending_machine as vm
 from vending_machine import VendingMachine
 
 class VendingMachineTest(unittest.TestCase):
@@ -13,16 +13,21 @@ class VendingMachineTest(unittest.TestCase):
         self.machine = VendingMachine()
 
     def test_is_valid_coin_with_nickel_should_return_true(self):
-        self.assertTrue(self.machine.is_valid_coin(vending_machine.NICKEL))
+        self.assertTrue(self.machine.is_valid_coin(vm.NICKEL))
 
     def test_is_valid_coin_with_dime_should_return_true(self):
-        self.assertTrue(self.machine.is_valid_coin(vending_machine.DIME))
+        self.assertTrue(self.machine.is_valid_coin(vm.DIME))
 
     def test_is_valid_coin_with_quarter_should_return_true(self):
-        self.assertTrue(self.machine.is_valid_coin(vending_machine.QUARTER))
+        self.assertTrue(self.machine.is_valid_coin(vm.QUARTER))
 
     def test_is_valid_coin_with_penny_should_return_false(self):
-        self.assertFalse(self.machine.is_valid_coin(vending_machine.PENNY))
+        self.assertFalse(self.machine.is_valid_coin(vm.PENNY))
+
+    def test_coin_values(self):
+        self.assertEqual(0.05, self.machine.VALID_COINS[vm.NICKEL])
+        self.assertEqual(0.10, self.machine.VALID_COINS[vm.DIME])
+        self.assertEqual(0.25, self.machine.VALID_COINS[vm.QUARTER])
 
 if __name__ == '__main__':
     unittest.main()
